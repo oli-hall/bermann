819290fb68c7200ecc82340acb89b43bdd18bfc7
Oli Hall <oliver.hall@duedil.com>
2017-10-23 17:41:41 +0100

    So many tests

--- bermann/rdd_test.py
+++ bermann/rdd_test.py
@@ -89,11 +89,27 @@ class TestRDD(unittest.TestCase):
 
     # groupByKey
 
-    # isEmpty
+    def test_isempty_returns_false_for_non_empty_rdd(self):
+        rdd = RDD([('k1', 'v1'), ('k1', 'v2'), ('k2', 'v3')])
+
+        self.assertFalse(rdd.isEmpty())
+
+    def test_isempty_returns_true_for_empty_rdd(self):
+        rdd = RDD()
+
+        self.assertTrue(rdd.isEmpty())
 
     # keyBy
 
-    # keys
+    def test_keys_returns_keys_of_kv_rdd(self):
+        rdd = RDD([('k1', 'v1'), ('k1', 'v2'), ('k2', 'v3')])
+
+        self.assertEqual(['k1', 'k1', 'k2'], rdd.keys().collect())
+
+    def test_keys_returns_first_elem_of_non_tuple_rdd(self):
+        rdd = RDD(['a', 'b', 'c'])
+
+        self.assertEqual(['a', 'b', 'c'], rdd.keys().collect())
 
     # map
 
@@ -103,7 +119,68 @@ class TestRDD(unittest.TestCase):
 
     # min
 
-    # name
+    def test_get_name_sets_name(self):
+        name = 'my RDD'
+        rdd = RDD([], name=name)
+
+        self.assertEqual(name, rdd.name)
+
+
+    # reduce
+
+    def test_set_name_sets_name(self):
+        rdd = RDD()
+
+        self.assertIsNone(rdd.name)
+
+        name = 'my_RDD'
+        rdd.setName(name)
+
+        self.assertEqual(name, rdd.name)
+
+    def test_sum_empty_rdd_returns_zero(self):
+        self.assertEqual(0, RDD([]).sum())
+
+    def test_sum_non_empty_rdd_returns_sum(self):
+        rdd = RDD([1, 2, 3])
+
+        self.assertEqual(6, rdd.sum())
+
+    def test_take_empty_rdd_returns_empty_list(self):
+        self.assertEqual([], RDD([]).take(10))
+
+    def test_take_short_rdd_returns_full_rdd(self):
+        rdd = RDD([1, 2, 3])
+
+        self.assertEqual(rdd.contents, rdd.take(10))
+
+    def test_take_long_rdd_returns_full_rdd(self):
+        rdd = RDD(list(range(20)))
+
+        self.assertEqual(rdd.contents[:10], rdd.take(10))
+
+    def test_union_of_empty_rdds_returns_empty_rdd(self):
+        self.assertEqual([], RDD([]).union(RDD([])).collect())
+
+    def test_union_of_rdds_returns_all_elements(self):
+        x = RDD([1, 2, 3])
+        y = RDD(['a', 'b', 'c', 'd'])
+
+        self.assertEqual([1, 2, 3, 'a', 'b', 'c', 'd'], x.union(y).collect())
+
+    def test_values_returns_value_of_kv_rdd(self):
+        rdd = RDD([('k1', 'v1'), ('k1', 'v2'), ('k2', 'v3')])
+
+        self.assertEqual(['v1', 'v2', 'v3'], rdd.values().collect())
+
+    def test_values_returns_second_elem_of_non_tuple_rdd(self):
+        rdd = RDD(['abc', 'def'])
+
+        self.assertEqual(['b', 'e'], rdd.values().collect())
+
+    # zip
+
+    # zipWithIndex
 
 
 if __name__ == '__main__':
