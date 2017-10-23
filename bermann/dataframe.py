c949d4bd4cb86cf9cfd532abb339ad4b575a96a2
Oli Hall <oliver.hall@duedil.com>
2017-10-23 19:29:54 +0100

    Initial version of DataFrame class

---
+++ bermann/dataframe.py
@@ -0,0 +1,239 @@
+from pyspark.storagelevel import StorageLevel
+
+
+class DataFrame(object):
+
+    def __init__(self, input, schema):
+        assert(input)
+        assert(schema)
+
+        # TODO validate input against schema?
+        # TODO infer schema?
+
+        self.input = input
+        self.schema = schema
+
+    def agg(self, *exprs):
+        raise NotImplementedError()
+
+    def alias(self, alias):
+        raise NotImplementedError()
+
+    def approxQuantile(self, col, probabilities, relativeError):
+        raise NotImplementedError()
+
+    def cache(self):
+        raise NotImplementedError()
+
+    def checkpoint(self, eager=True):
+        raise NotImplementedError()
+
+    def coalesce(self, numPartitions):
+        raise NotImplementedError()
+
+    def collect(self):
+        raise NotImplementedError()
+
+    def columns(self):
+        raise NotImplementedError()
+
+    def corr(self, col1, col2, method=None):
+        raise NotImplementedError()
+
+    def count(self):
+        raise NotImplementedError()
+
+    def cov(self, col1, col2):
+        raise NotImplementedError()
+
+    def createGlobalTempView(self, name):
+        raise NotImplementedError()
+
+    def createOrReplaceGlobalTempView(self, name):
+        raise NotImplementedError()
+
+    def createOrReplaceTempView(self, name):
+        raise NotImplementedError()
+
+    def createTempView(self, name):
+        raise NotImplementedError()
+
+    def crossJoin(self, other):
+        raise NotImplementedError()
+
+    def crosstab(self, col1, col2):
+        raise NotImplementedError()
+
+    def cube(self, *cols):
+        raise NotImplementedError()
+
+    def describe(self, *cols):
+        raise NotImplementedError()
+
+    def distinct(self):
+        raise NotImplementedError()
+
+    def drop(self, *cols):
+        raise NotImplementedError()
+
+    def dropDuplicates(self, subset=None):
+        raise NotImplementedError()
+
+    def drop_duplicates(self, subset=None):
+        raise NotImplementedError()
+
+    def dropna(self, how='any', thresh=None, subset=None):
+        raise NotImplementedError()
+
+    def explain(self, extended=False):
+        raise NotImplementedError()
+
+    def fillna(self, value, subset=None):
+        raise NotImplementedError()
+
+    def filter(self, condition):
+        raise NotImplementedError()
+
+    def first(self):
+        raise NotImplementedError()
+
+    def foreach(self, f):
+        raise NotImplementedError()
+
+    def foreachPartition(self, f):
+        raise NotImplementedError()
+
+    def freqItems(self, cols, support=None):
+        raise NotImplementedError()
+
+    def groupBy(self, *cols):
+        raise NotImplementedError()
+
+    def groupby(self, *cols):
+        raise NotImplementedError()
+
+    def head(self, n=None):
+        raise NotImplementedError()
+
+    def hint(self, name, *parameters):
+        raise NotImplementedError()
+
+    def intersect(self, other):
+        raise NotImplementedError()
+
+    def isLocal(self, ):
+        raise NotImplementedError()
+
+    def isStreaming(self, ):
+        raise NotImplementedError()
+
+    def join(self, other, on=None, how=None):
+        raise NotImplementedError()
+
+    def limit(self, num):
+        raise NotImplementedError()
+
+    def na(self):
+        raise NotImplementedError()
+
+    def orderBy(self, *cols, **kwargs):
+        raise NotImplementedError()
+
+    def persist(self, storageLevel=StorageLevel(True, True, False, False, 1)):
+        raise NotImplementedError()
+
+    def printSchema(self, ):
+        raise NotImplementedError()
+
+    def randomSplit(self, weights, seed=None):
+        raise NotImplementedError()
+
+    def rdd(self):
+        raise NotImplementedError()
+
+    def registerTempTable(self, name):
+        raise NotImplementedError()
+
+    def repartition(self, numPartitions, *cols):
+        raise NotImplementedError()
+
+    def replace(self, to_replace, value=None, subset=None):
+        raise NotImplementedError()
+
+    def rollup(self, *cols):
+        raise NotImplementedError()
+
+    def sample(self, withReplacement, fraction, seed=None):
+        raise NotImplementedError()
+
+    def sampleBy(self, col, fractions, seed=None):
+        raise NotImplementedError()
+
+    def schema(self):
+        raise NotImplementedError()
+
+    def select(self, *cols):
+        raise NotImplementedError()
+
+    def selectExpr(self, *expr):
+        raise NotImplementedError()
+
+    def show(self, n=20, truncate=True):
+        raise NotImplementedError()
+
+    def sort(self, *cols, **kwargs):
+        raise NotImplementedError()
+
+    def sortWithinPartitions(self, *cols, **kwargs):
+        raise NotImplementedError()
+
+    def stat(self):
+        raise NotImplementedError()
+
+    def storageLevel(self, ):
+        raise NotImplementedError()
+
+    def subtract(self, other):
+        raise NotImplementedError()
+
+    def take(self, num):
+        raise NotImplementedError()
+
+    def toDF(self, *cols):
+        raise NotImplementedError()
+
+    def toJSON(self, use_unicode=True):
+        raise NotImplementedError()
+
+    def toLocalIterator(self, ):
+        raise NotImplementedError()
+
+    def toPandas(self):
+        raise NotImplementedError()
+
+    def union(self, other):
+        raise NotImplementedError()
+
+    def unionAll(self, other):
+        raise NotImplementedError()
+
+    def unpersist(self, blocking=False):
+        raise NotImplementedError()
+
+    def where(self, condition):
+        raise NotImplementedError()
+
+    def withColumn(self, colName, col):
+        raise NotImplementedError()
+
+    def withColumnRenamed(self, existing, new):
+        raise NotImplementedError()
+
+    def withWatermark(self, eventTime, delayThreshold):
+        raise NotImplementedError()
+
+    def write(self):
+        raise NotImplementedError()
+
+    def writeStream(self):
+        raise NotImplementedError()
